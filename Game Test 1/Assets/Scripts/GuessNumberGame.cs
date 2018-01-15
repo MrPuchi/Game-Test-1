ac30121c83dfc8e2bfe131cc25addd3e120a18b2
MrPuchi <juanzacarias0@gmail.com>
2018-01-15 00:47:59 -0500

    Final Game

--- Game Test 1/Assets/Scripts/GuessNumberGame.cs
+++ Game Test 1/Assets/Scripts/GuessNumberGame.cs
@@ -1,16 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class GuessNumberGame : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public InputField Input;
+    public Text InfoText;
+    private int guessnumber;
+    private int userguess;
+    private Text PH;
+
+    // Use this for initialization
+    void Start()
+    {
+        guessnumber = Random.Range(0, 100);
+        Input.ActivateInputField();
+        PH = Input.placeholder.GetComponent<Text>();
+
+    }
+	public void checkguess()
+    {
+        PH.text = "Keep trying!";
+        if (Input.text == "")
+        {
+            InfoText.text = "You have to enter a number";
+            Input.text = "";
+            Input.ActivateInputField();
+            return;
+        }
+        userguess = int.Parse(Input.text);
+
+        if (userguess == guessnumber)
+        {
+            InfoText.text = "You are Wizard!! The number is " + guessnumber;
+            return;
+        }
+        else if (userguess > guessnumber)
+        {
+            InfoText.text = "You number is greater than the guess number";
+        }
+        else
+        {
+            InfoText.text = "You number is less than the guess number";
+        }
+        Input.text = "";
+        Input.ActivateInputField();
+
+    }   
+        
+    
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
